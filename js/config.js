e30ee3be02d14c85ca6b79fad71cdf1b3d47ca70
leeoo8 <leeoo888@outlook.com>
2025-04-11 15:38:03 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // 全局常量配置
-const PROXY_URL = 'https://soft-glade-24bd.snowy-pond-ab0a.workers.dev/';
+const PROXY_URL = 'https://cors.zme.ink/';
 const HOPLAYER_URL = 'https://hoplayer.com/index.html';
 const SEARCH_HISTORY_KEY = 'videoSearchHistory';
 const MAX_HISTORY_ITEMS = 5;
