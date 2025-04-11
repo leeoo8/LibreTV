9fea28fd55d21e9ba26f27ea47e3de97e4d69aa4
leeoo8 <leeoo888@outlook.com>
2025-04-11 15:11:55 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // 全局常量配置
-const PROXY_URL = 'https://cors.zme.ink/';
+const PROXY_URL = 'https://y.demo.lhyang.org/';
 const HOPLAYER_URL = 'https://hoplayer.com/index.html';
 const SEARCH_HISTORY_KEY = 'videoSearchHistory';
 const MAX_HISTORY_ITEMS = 5;
